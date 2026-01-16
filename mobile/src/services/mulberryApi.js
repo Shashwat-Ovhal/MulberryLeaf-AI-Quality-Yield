8b87e86ca37284e2cf25257ff487fb86e8ed4184
Shashwat-Ovhal <o.shashwat14@gmail.com>
2026-01-16 12:53:08 +0530

    increased app's patience (timeout) to 60 seconds.

--- mobile/src/services/mulberryApi.js
+++ mobile/src/services/mulberryApi.js
@@ -12,7 +12,7 @@ const BASE_URL = getApiUrl();
 
 const api = axios.create({
     baseURL: BASE_URL,
-    timeout: 10000,
+    timeout: 60000,
     headers: {
         'Content-Type': 'application/json',
     },
